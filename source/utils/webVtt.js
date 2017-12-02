1cc81fa6368ba651fbce805b51391ca5708debfe
andrey_abramov <andrew.abramov00@gmail.com>
2017-12-02 16:47:46 +0300

    empty vtt

--- source/utils/webVtt.js
+++ source/utils/webVtt.js
@@ -3,11 +3,13 @@ import {msToString} from './time';
 export const commentsToWebVttBlobUrl = (comments) => {
   let webVttText = `WEBVTT\r\n\r\n`;
 
-  comments.forEach((comment) => {
-    webVttText +=
-      `${msToString(comment.inPoint)} --> ${msToString(comment.outPoint)}
+  if (comments) {
+    comments.forEach((comment) => {
+      webVttText +=
+        `${msToString(comment.inPoint)} --> ${msToString(comment.outPoint)}
 ${comment.text}\r\n\r\n`;
-  });
+    });
+  }
   const blob = new Blob([webVttText], {
     type: 'text/vtt',
   });
