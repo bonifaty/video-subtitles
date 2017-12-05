8045a6f7ce9e51609b45e24bcb6c2621e87e1af8
andrey_abramov <andrew.abramov00@gmail.com>
2017-12-05 10:55:11 +0300

    fin

--- source/components/video-player/index.js
+++ source/components/video-player/index.js
@@ -11,14 +11,10 @@ class VideoPlayer extends Component {
   constructor(props) {
     super();
     this.state = {
-      isPlaying: false,
       currentTime: 0,
     };
     this.duration = Math.ceil(props.duration * 1000);
-    this.handlePlayPauseClick = this.handlePlayPauseClick.bind(this);
     this.handleTimeUpdate = this.handleTimeUpdate.bind(this);
-    this.updateTimeline = this.updateTimeline.bind(this);
-    this.pauseVideo = this.pauseVideo.bind(this);
   }
 
   handleTimeUpdate(e) {
@@ -27,32 +23,7 @@ class VideoPlayer extends Component {
     });
   }
 
-  pauseVideo() {
-    this._video.pause();
-    this.setState({
-      isPlaying: false,
-    });
-  }
-
-  handlePlayPauseClick() {
-    if (this.state.isPlaying) {
-      this._video.pause();
-    } else {
-      this._video.play();
-    }
-    this.setState({
-      isPlaying: !this.state.isPlaying,
-    });
-  }
-
-  updateTimeline(e) {
-    this._video.currentTime = e.target.value;
-    if (this.state.isPlaying) {
-      this.pauseVideo();
-    }
-  }
-
-  render({subtitlesUrl, videoUrl}, {isPlaying, currentTime}) {
+  render({subtitlesUrl, videoUrl}, {currentTime}) {
     return (<div className={b()}>
       <div className={b('container')}>
         <video controls ref={(c) => this._video = c} src={videoUrl}
